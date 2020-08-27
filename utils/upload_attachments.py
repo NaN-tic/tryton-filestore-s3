76222968443a8221ed2feb92ffeaf8a1225375e7
Àngel Àlvarez <angel@nan-tic.com>
2020-08-27 10:38:34 +0200

    Migrate to boto3

--- utils/upload_attachments.py
+++ utils/upload_attachments.py
@@ -10,13 +10,17 @@ import os
 import warnings
 from optparse import OptionParser
 
-from boto.s3.connection import S3Connection, Location
-from boto.s3.key import Key
+# from boto.s3.connection import S3Connection, Location
+# from boto.s3.key import Key
+
+import boto3
+import uuid
 
 
-def upload(connection, bucket, data_path, database, new_db_name=None):
+
+def upload(client, bucket, data_path, database, new_db_name=None):
     """
-    :param connection: The Amazon S3 connection
+    :param client: The Amazon S3 connection
     :param bucket: The name of the bucket to which the data has to be uploaded
     :param data_path: The data_path used by Tryton
     :param database: The name of the database
@@ -24,7 +28,7 @@ def upload(connection, bucket, data_path, database, new_db_name=None):
                         This DB name will be used as prefix instead of the
                         existing db name.
     """
-    bucket = connection.get_bucket(bucket)
+#    bucket = connection.get_bucket(bucket)
 
     if new_db_name is None:
         new_db_name = database
@@ -40,16 +44,9 @@ def upload(connection, bucket, data_path, database, new_db_name=None):
         counter['directories'] += 1
         for filename in filenames:
             filepath = os.path.join(dirpath, filename)
-
-            if bucket.get_key(filename):
-                warnings.warn("Duplicate file. Ignore: %s" % filepath)
-                counter['ignored'] += 1
-                continue
-
-            s3_object = Key(bucket)
-            s3_object.key = "%s/%s" % (new_db_name, filename)
-            s3_object.set_contents_from_filename(filepath)
-
+            key = "%s/%s" % (new_db_name, filename)
+            client.upload_file(filepath, bucket, key,
+                ExtraArgs={'StorageClass': 'STANDARD_IA'})
             counter['uploaded'] += 1
             print( "[%d]: File %s uploaded to S3" % (
                 counter['uploaded'], filename)
@@ -67,7 +64,7 @@ if __name__ == '__main__':
         )
     )
     parser.add_option("-n", "--new-db-name", dest="new_db_name",
-        help="New database name to prefix to the files", default=None)
+        help="New database name to x to the files", default=None)
 
     (options, args) = parser.parse_args()
 
@@ -75,5 +72,8 @@ if __name__ == '__main__':
         parser.error("Invalid options")
 
     REGION_HOST = 's3.eu-west-3.amazonaws.com'
-    connection = S3Connection(*args[:2],  host=REGION_HOST)
-    upload(connection, new_db_name=options.new_db_name, *args[2:])
+
+    client = boto3.client('s3',
+        aws_access_key_id = args[0],
+        aws_secret_access_key = args[1])
+    upload(client, new_db_name=options.new_db_name, *args[2:])
